8532b53d478710b50a0225793b9cabf5073539ae
remy <rem59570@gmail.com>
2024-03-29 15:25:43 +0100

    Add Eureka

---
+++ back/eureka/src/test/java/com/fisa/eureka/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fisa.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

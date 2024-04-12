ee3553f5bfe5e8ee07a5bce0678958a98b287e36
Elogrz <eloise.26@hotmail.fr>
2024-04-12 15:00:48 +0200

    API Authentification Spring Security
    3.2.4

--- back/authentification/src/main/java/client/api/authentification/config/SecurityConfig.java
+++ back/authentification/src/main/java/client/api/authentification/config/SecurityConfig.java
@@ -2,40 +2,27 @@ package client.api.authentification.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import client.api.authentification.service.ClientDetailsService;
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
-    @Autowired
-    private ClientDetailsService clientDetailsService;
-
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(clientDetailsService);
-    }
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable()
-                .authorizeRequests()
-                .antMatchers("/auth/login").permitAll()
-                .anyRequest().authenticated()
-                .and()
-                .formLogin().loginProcessingUrl("/auth/login");
-    }
-
+public class SecurityConfig {
     @Bean
-    public PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((authz) -> authz
+                        .requestMatchers("/auth/login").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin(form -> form
+                        .loginPage("/login")
+                        .permitAll()
+                )
+                .httpBasic(withDefaults());
+        return http.build();
     }
 }

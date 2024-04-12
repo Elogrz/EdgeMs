ffc2e15a1191ab9cdb0a82afca184ede01209cb5
MathieuSymphorien <mathieu.symphorien@etu.univ-amu.fr>
2024-04-12 12:11:38 +0200

    ajout client 1

--- back/product/src/main/java/product/api/product/ProductApplication.java
+++ back/product/src/main/java/product/api/product/ProductApplication.java
@@ -3,7 +3,13 @@ package product.api.product;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// import com.mproduits.configurations.ApplicationPropertiesConfiguration;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
 @SpringBootApplication
+@EnableConfigurationProperties
+@EnableDiscoveryClient
 public class ProductApplication {
 
 	public static void main(String[] args) {

9b03016fc1130b4d88d257ffeac37ac71ac57166
Elogrz <eloise.26@hotmail.fr>
2024-04-12 16:52:28 +0200

    test config

--- back/authentification/src/main/java/client/api/authentification/AuthentificationApplication.java
+++ back/authentification/src/main/java/client/api/authentification/AuthentificationApplication.java
@@ -2,11 +2,12 @@ package client.api.authentification;
 
 import client.api.authentification.config.SecurityConfig;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.Import;
-
+@EnableAutoConfiguration
 @SpringBootApplication
-@Import(SecurityConfig.class)
 public class AuthentificationApplication {
 
 	public static void main(String[] args) {

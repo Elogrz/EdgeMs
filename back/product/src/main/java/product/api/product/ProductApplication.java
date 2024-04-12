f6675a35f768639c3c715e898e04584e68131d3f
MathieuSymphorien <mathieu.symphorien@etu.univ-amu.fr>
2024-04-12 14:47:49 +0200

    client 2

--- back/product/src/main/java/product/api/product/ProductApplication.java
+++ back/product/src/main/java/product/api/product/ProductApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 // import com.mproduits.configurations.ApplicationPropertiesConfiguration;
+
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 

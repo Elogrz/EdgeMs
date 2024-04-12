d3a74f6780fd4a0ae2d10d43ad8c368947cdba02
Elogrz <eloise.26@hotmail.fr>
2024-04-12 19:35:05 +0200

    config authentification

--- back/authentification/src/main/java/client/api/authentification/AuthentificationApplication.java
+++ back/authentification/src/main/java/client/api/authentification/AuthentificationApplication.java
@@ -6,9 +6,11 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Import;
+import org.springframework.web.bind.annotation.RestController;
 
 @EntityScan("client.api.authentification.model")
 @EnableAutoConfiguration
+@RestController
 @SpringBootApplication
 public class AuthentificationApplication {
 

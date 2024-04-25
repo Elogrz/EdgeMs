46158b321706931e00d19dcc2db66135099d8a43
Elogrz <eloise.26@hotmail.fr>
2024-04-25 21:27:42 +0200

    Sécurité config
    authentification

--- back/authentification/src/main/java/client/api/authentification/config/SecurityConfig.java
+++ back/authentification/src/main/java/client/api/authentification/config/SecurityConfig.java
@@ -6,7 +6,10 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -19,19 +22,26 @@ public class SecurityConfig {
         http
                 .authenticationManager(authenticationManager)
                 .authorizeHttpRequests((authz) -> authz
-                        .requestMatchers("/auth/login").permitAll()
+                        .requestMatchers("/auth/login","/auth/register").permitAll()
                         .anyRequest().authenticated()
                 )
                 .formLogin(form -> form
                         .loginPage("/login")
                         .permitAll()
                 )
-                .httpBasic(withDefaults());
+                .httpBasic(withDefaults())
+                .csrf((csrf) -> csrf.ignoringRequestMatchers(new AntPathRequestMatcher("/auth/register")))
+                .csrf((csrf) -> csrf.ignoringRequestMatchers(new AntPathRequestMatcher("/auth/login")));
         return http.build();
     }
 
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
         return authConfig.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }

0e8e6f13ab64102fd2d947f232f54c353377739d
Elogrz <eloise.26@hotmail.fr>
2024-04-25 17:53:58 +0200

    fix docker authentification

--- back/authentification/src/main/java/client/api/authentification/config/SecurityConfig.java
+++ back/authentification/src/main/java/client/api/authentification/config/SecurityConfig.java
@@ -2,6 +2,8 @@ package client.api.authentification.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -11,9 +13,11 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain filterChain(HttpSecurity http, AuthenticationManager authenticationManager) throws Exception {
         http
+                .authenticationManager(authenticationManager)
                 .authorizeHttpRequests((authz) -> authz
                         .requestMatchers("/auth/login").permitAll()
                         .anyRequest().authenticated()
@@ -24,5 +28,10 @@ public class SecurityConfig {
                 )
                 .httpBasic(withDefaults());
         return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
+        return authConfig.getAuthenticationManager();
     }
 }

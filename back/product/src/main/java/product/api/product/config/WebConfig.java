245e471fee43a460c220f90010a58ed5ac39afa4
Aurélien Dufour <aureluendufour7@gmail.com>
2024-04-25 17:48:04 +0200

    merge main

--- back/product/src/main/java/product/api/product/config/WebConfig.java
+++ back/product/src/main/java/product/api/product/config/WebConfig.java
@@ -1,14 +1,18 @@
+package product.api.product.config;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
+                .allowedOrigins("*") // Autoriser toutes les origines
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*");
     }

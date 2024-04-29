fe85e5ddd06bc8bfccfbfa0f10605bd10fab8718
remy <rem59570@gmail.com>
2024-04-29 19:06:45 +0200

    Implement payments API

--- back/payments/src/main/java/com/fisa/payments/controller/PaymentController.java
+++ back/payments/src/main/java/com/fisa/payments/controller/PaymentController.java
@@ -1,2 +1,19 @@
-package com.fisa.payments.controller;public class PaymentController {
+package com.fisa.payments.controller;
+
+import com.fisa.payments.model.Payment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/payment")
+public class PaymentController {
+
+    @PostMapping
+    public ResponseEntity<Boolean> createCustomerOrder(@RequestBody Payment payment) {
+        return new ResponseEntity<>( payment.isPaymentValide(), HttpStatus.ACCEPTED);
+    }
 }
--- back/payments/src/main/java/com/fisa/payments/model/Payment.java
+++ back/payments/src/main/java/com/fisa/payments/model/Payment.java
@@ -1,2 +1,18 @@
-package com.fisa.payments.model;public class payment {
+package com.fisa.payments.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Payment {
+    public Integer amount;
+
+    public boolean isPaymentValide(){
+        return amount.intValue() < 100;
+    }
 }
